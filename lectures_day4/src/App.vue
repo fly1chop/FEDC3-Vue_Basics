14453c31cd90951b4d5092065e9ae75f06a8df2e
Jane Ro <janero95@gmail.com>
2022-12-06 23:12:06 +0900

    vue SFC setup feat. Parcel

---
+++ lectures_day4/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello Vue!',
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+$color: orange;
+h1 {
+  color: $color;
+}
+</style>
---
+++ lectures_day4/src/main.js
@@ -0,0 +1,5 @@
+import * as Vue from 'vue'
+import App from './App.vue'
+
+
+Vue.createApp(App).mount('#app')
