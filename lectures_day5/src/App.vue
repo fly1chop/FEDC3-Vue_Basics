d1046f1308c6b53b1197145f2fc03e42610e9682
Jane Ro <janero95@gmail.com>
2022-12-08 01:28:27 +0900

    using $nextTick() to pass callback after state change

--- lectures_day5/src/App.vue
+++ lectures_day5/src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <h1>{{ msg }}</h1>
-  <Hello ref="hola" />
+  <div v-if="!isEdit">
+    {{ msg }}
+    <button @click="onEdit">Edit!</button>
+  </div>
+  <div v-else>
+    <input
+      ref="editor" 
+      v-model="msg"
+      @keyup.enter="isEdit = false" />
+  </div>
 </template>
 
 <script>
-import Hello from '~/components/Hello'
 
 export default {
-  components: {
-    Hello
-  },
   data() {
     return {
+      isEdit: false,
       msg: 'Hello Vue!'
     }
   },
-  mounted() {
-    // To select actual DOM element from Vue component, you need to look into $el property
-    // console.log(this.$refs.hola.$el)
-
-    // When there are multiple refs in component
-    console.log(this.$refs.hola.$refs.hello)
+  methods: {
+    onEdit() {
+      this.isEdit = true
+      this.$nextTick(() => {
+        // use nextTick to pass callback to occur after next DOM update is complete
+        this.$refs.editor.focus()
+      })
+      // setTimeout(() => {
+      //   // forcing this step to call stack with setTimeout
+      //   this.$refs.editor.focus()
+      // })
+    }
   }
 }
 </script>
--- lectures_day5/src/components/Hello.vue
+++ lectures_day5/src/components/Hello.vue
@@ -5,12 +5,7 @@
 
 <script>
 export default {
-  mounted() {
-    // const $h1 = document.querySelector('h1')
-    // cannot use $refs to find element in created() of lifecycle because at that point, only js is initialized and not yet connected with html (template)
-    const $h1 = this.$refs.hello
-    console.log($h1)
-  }
+  
 }
 </script>
 
