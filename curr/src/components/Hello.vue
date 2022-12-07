83c386b8bef262b906ce34d6ef81afcea591187d
Jane Ro <janero95@gmail.com>
2022-12-07 18:23:42 +0900

    input binding between components using $emit

--- curr/src/components/Hello.vue
+++ curr/src/components/Hello.vue
@@ -1,25 +1,17 @@
 <template>
-  <div>
-    <h1 @click="$emit('please', 9)">
-      Please
-    </h1>
-    <h1 @click="$emit('click')">
-      Click
-    </h1>
-  </div>
+  <label>
+    <input
+      :value="message"
+      @input="$emit('update', $event.target.value)" />
+  </label>
 </template>
 
 <script>
 export default {
-  emits: {
-    click: null,
-    please: (num) => {
-      if(num > 10) {
-        return true
-      } else {
-        console.log('num is not >10')
-        return false
-      }
+  props: {
+    message: {
+      type: String,
+      default: ''
     }
   }
 }
