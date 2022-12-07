9cf6eda64067044f99d6c91a52a9061fb1efe7ed
Jane Ro <janero95@gmail.com>
2022-12-07 23:20:22 +0900

    using <slot> and setting fallback content

---
+++ lectures_day5/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <Hello>
+    <h2>Hello Vue?</h2>
+    <p>The brown fox!</p>
+  </Hello>
+  <Btn>Click Me</Btn>
+  <Btn></Btn>
+</template>
+
+<script>
+import Hello from '~/components/Hello'
+
+export default {
+  components: {
+    Hello,
+  },
+  data() {
+    return {
+      msg: 'Hello vue! & webpack?',
+    }
+  },
+}
+</script>
---
+++ lectures_day5/src/components/Btn.vue
@@ -0,0 +1,16 @@
+<template>
+  <button>
+    <slot>Fallback Content</slot>
+  </button>
+</template>
+
+<style scoped lang="scss">
+button {
+  padding: 10px 20px;
+
+  &:hover {
+    background-color: orange;
+    transform: scale(1.2);
+  }
+}
+</style>
